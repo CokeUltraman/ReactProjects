485c92aa20ef91391ba0e21db44fed82def90f02
cokeultraman <tonyzeng12@gmail.com>
2021-07-16 19:25:01 -0400

    axios搜索github

--- react_staging/src/components/Search/index.jsx
+++ react_staging/src/components/Search/index.jsx
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
-
+import axios from 'axios'
 export default class Search extends Component {
+
+    search=()=>{
+        //获取用户的输入(解构赋值的连续写法+重命名)
+        const {keyWordElement:{value:keyWord}}=this
+        //console.log(keyWord)
+        //发送网络请求
+        axios.get(`api1/search/users?q=${keyWord}`).then(
+            response=>{console.log('success',response.data);},
+            error=>{console.log('fail',error);}
+        )
+
+    }
+
     render() {
         return (
             <section className="jumbotron">
                 <h3 className="jumbotron-heading">Search Github Users</h3>
                 <div>
-                    <input type="text" placeholder="enter the name you search"/>&nbsp;<button>Search</button>
+                    <input ref={c=>this.keyWordElement=c} type="text" placeholder="enter the name you search"/>&nbsp;
+                    <button onClick={this.search}>Search</button>
                 </div>
                 </section>
         )
