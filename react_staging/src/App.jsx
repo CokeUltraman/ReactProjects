b2ee1bfdcf3370e782503f4adcf178a4d6029868
cokeultraman <tonyzeng12@gmail.com>
2021-07-19 23:48:43 -0400

    update

--- react_staging/src/App.jsx
+++ react_staging/src/App.jsx
@@ -5,8 +5,10 @@ import List from './components/List'
 
 export default class App extends Component {
     
-    state={state:[users:]}//初始化状态，users初始值为数组.
-    
+    state={users:[]}//初始化状态，users初始值为数组.
+    saveUsers=(users)=>{
+        this.setState({users:users})
+    }
     render() {
         return (
             <div className="container">
