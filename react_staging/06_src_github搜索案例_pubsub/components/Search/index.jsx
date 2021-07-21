8eb2704dad89bf04924c6a0d9f8a094ab6a4c7e0
cokeultraman <tonyzeng12@gmail.com>
2021-07-21 02:52:56 -0400

    fetch发送请求01

---
+++ react_staging/06_src_github搜索案例_pubsub/components/Search/index.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import PubSub from 'pubsub-js'
+import axios from 'axios'
+export default class Search extends Component {
+
+    search=()=>{
+        
+        //获取用户的输入(解构赋值的连续写法+重命名)
+        const {keyWordElement:{value:keyWord}}=this
+        //发送请求前通知List更新状态
+        // this.props.updateAppState({isFirst:false,isLoading:true})
+        PubSub.publish("aaa",{isFirst:false,isLoading:true})
+        //console.log(keyWord)
+        //#region 发送网络请求---使用axios发送
+        // axios.get(`api1/search/users?q=${keyWord}`).then(
+        //     response=>{
+        //         //请求成功后通知List更新状态
+        //         // this.props.updateAppState({isLoading:false,users:response.data.items})
+        //         PubSub.publish("aaa",{isLoading:false,users:response.data.items})
+        //     },
+
+        //     error=>{
+        //         //请求失败后通知App更新状态
+        //         //this.props.updateAppState({isLoading:false,err:error.message})
+        //         PubSub.publish("aaa",{isLoading:false,err:error.message})
+        //     }
+        // )
+        //#endregion
+
+        //发送网络请求 fetch发送
+          fetch('api1/search/users?q=${keyWord}').then(
+              response=>{},
+              error=>{   }
+          )
+    }
+
+    render() {
+        return (
+            <section className="jumbotron">
+                <h3 className="jumbotron-heading">Search Github Users</h3>
+                <div>
+                    <input ref={c=>this.keyWordElement=c} type="text" placeholder="enter the name you search"/>&nbsp;
+                    <button onClick={this.search}>Search</button>
+                </div>
+                </section>
+        )
+    }
+}
