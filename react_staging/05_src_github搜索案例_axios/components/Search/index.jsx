a91c5b03c225245c851b4d49456fc340f26f4b65
cokeultraman <tonyzeng12@gmail.com>
2021-07-20 23:49:51 -0400

    消息订阅与发布
    subscribe and publish

--- react_staging/05_src_github搜索案例_axios/components/Search/index.jsx
+++ react_staging/05_src_github搜索案例_axios/components/Search/index.jsx
@@ -1,28 +1,22 @@
 import React, { Component } from 'react'
-import PubSub from 'pubsub-js'
 import axios from 'axios'
 export default class Search extends Component {
 
     search=()=>{
-        
         //获取用户的输入(解构赋值的连续写法+重命名)
         const {keyWordElement:{value:keyWord}}=this
-        //发送请求前通知List更新状态
-        // this.props.updateAppState({isFirst:false,isLoading:true})
-        PubSub.publish("aaa",{isFirst:false,isLoading:true})
+        //发送请求前通知App更新状态
+        this.props.updateAppState({isFirst:false,isLoading:true})
         //console.log(keyWord)
         //发送网络请求
         axios.get(`api1/search/users?q=${keyWord}`).then(
             response=>{
-                //请求成功后通知List更新状态
-                // this.props.updateAppState({isLoading:false,users:response.data.items})
-                PubSub.publish("aaa",{isLoading:false,users:response.data.items})
-            },
+                //请求成功后通知App更新状态
+                this.props.updateAppState({isLoading:false,users:response.data.items})},
 
             error=>{
                 //请求失败后通知App更新状态
-                //this.props.updateAppState({isLoading:false,err:error.message})
-                PubSub.publish("aaa",{isLoading:false,err:error.message})
+                this.props.updateAppState({isLoading:false,err:error.message})
             }
         )
 
--- react_staging/05_src_github搜索案例_axios/setupProxy.js
+++ react_staging/05_src_github搜索案例_axios/setupProxy.js
@@ -6,12 +6,8 @@ module.exports=function(app){
             target:'http://localhost:5000',//请求转发给谁
             changeOrigin:true,//控制服务器收到的响应中host字段的值
             pathRewrite:{'^/api1':''}//重写请求路径
-        }),
-        proxy('/api2',{
-            target:'http://localhost:5001',
-            changeOrigin:true,
-            pathRewrite:{'^/api2':''}
         })
+        
     )
     
 
