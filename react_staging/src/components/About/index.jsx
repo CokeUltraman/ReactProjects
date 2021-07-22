23e5249d68dc8f555ee3d0f60af4a984743350ad
cokeultraman <tonyzeng12@gmail.com>
2021-07-22 03:09:55 -0400

    路由基本使用03

--- react_staging/src/components/About/index.jsx
+++ react_staging/src/components/About/index.jsx
@@ -3,9 +3,7 @@ import React, { Component } from 'react'
 export default class About extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <h3>我是Home的内容</h3>
         )
     }
 }
