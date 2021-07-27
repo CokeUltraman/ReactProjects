2a8f2a8b789726237087ec51c022753178ee608d
cokeultraman <tonyzeng12@gmail.com>
2021-07-26 21:54:50 -0400

    update

--- react_staging/src/pages/Home/Message/index.jsx
+++ react_staging/src/pages/Home/Message/index.jsx
@@ -1,19 +1,28 @@
 import React, { Component } from 'react'
 
 export default class Message extends Component {
+    state={
+        messageArr:[
+            {id:'01',title:'news 01'},
+            {id:'02',title:'news 02'},
+            {id:'03',title:'news 03'},
+
+        ]
+    }
+    
+    
     render() {
+        const {messageArr}=this.state
         return (
             <div>
                   <ul>
-                    <li>
-                      <a href="/message1">message001</a>&nbsp;&nbsp;
-                    </li>
-                    <li>
-                      <a href="/message2">message002</a>&nbsp;&nbsp;
-                    </li>
-                    <li>
-                      <a href="/message/3">message003</a>&nbsp;&nbsp;
-                    </li>
+                    {
+                        messageArr.map(()=>{
+                            return(
+                                
+                            )
+                        })
+                    }
                   </ul>
             </div>
         )
