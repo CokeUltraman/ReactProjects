7b1c389d888c6478b66f87cdc5a08e8d3aa90f58
cokeultraman <tonyzeng12@gmail.com>
2021-08-07 00:21:41 -0400

    statehook

--- react_extension/src/components/3_hooks/index.jsx
+++ react_extension/src/components/3_hooks/index.jsx
@@ -1,18 +1,35 @@
 import React, { Component } from 'react'
-
-export default class Demo extends Component {
+//类式组件
+// export default class Demo extends Component {
     
-    state = {count:0}
+//     state = {count:0}
 
-    add=()=>{
-        this.setState(state=>({count:state.count+1}))
+//     add=()=>{
+//         this.setState(state=>({count:state.count+1}))
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <h2>当前求和为{this.state.count}</h2>
+//                 <button onClick={this.add}>touch plus one</button>
+//             </div>
+//         )
+//     }
+// }
+
+//函数式组件
+function Demo(){
+    const [count,setCount]=React.useState(0)   
+   //加的回调
+    function add(){
+        //setCount(count+1)//第一种写法
+        setCount(count=>count+1)
     }
-    render() {
-        return (
-            <div>
-                <h2>当前求和为{this.state.count}</h2>
-                <button onClick={this.add}>touch plus one</button>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <h2>当前求和为{this.state.count}</h2>
+            <button onClick={add}>touch plus one</button>
+        </div>
+    )
 }
+export default Demo
