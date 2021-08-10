e2bfa615a7d0dc4bc275982189d4891bc852e1ec
cokeultraman <tonyzeng12@gmail.com>
2021-08-10 00:48:58 -0400

    enddddddddddddd

--- react_extension/src/components/8_ErrorBoundary/Parent.jsx
+++ react_extension/src/components/8_ErrorBoundary/Parent.jsx
@@ -1,15 +1,25 @@
  import React, { Component } from 'react'
  import Child from './Child'
-
+ 
+ 
+  
  export default class Parent extends Component {
-     
     
+    state={
+        hasError:''//用于标识子组件是否产生错误
+    }
+    
+    //当parent的子组件出现报错的时候，会触发getDerivedStateFromError调用，并携带错误信息
+    static getDerivedStateFromError(error){
+        return {hasError:error}
+    }
     
     render() {
          return (
              <div>
                  <h2>I am parent component</h2>
-                 <Child/>
+                 
+                 {this.state.hasError? <h2>internet erro,please try later</h2>:<Child/>} 
              </div>
          )
      }
