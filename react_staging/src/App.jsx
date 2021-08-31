db7ea5e0da06f9d833babdaa8bbf3e70154b65ab
cokeultraman <tonyzeng12@gmail.com>
2021-08-30 23:50:59 -0400

    test

--- react_staging/src/App.jsx
+++ react_staging/src/App.jsx
@@ -1,13 +1,51 @@
 import React, { Component } from 'react'
-import {Button} from 'antd' ;
-import 'antd/dist/antd.css'
+import {Route,Switch,Redirect} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'    //Home About是路由组件
+import Header from './components/Header'  //Header是一般组件
+import MyNavLink from './components/MyNavLink'
 export default class App extends Component {
     render() {
         return (
             <div>
-                App...
-                <button>touch me </button>
-                <Button type='primary'>Primary Button</Button>
+                <div className="row">
+                    <div className="col-xs-offset-2 col-xs-8">
+                        <Header/>
+                    </div>
+                    
+                    <div className="row">
+                        <div className="col-xs-2 col-xs-offset-2">
+                            <div className="list-group">
+                                {/* 原生html中，靠<a>跳转不同的页面 */}
+                                {/* <a className="list-group-item" href="./about.html">About</a>
+                                <a className="list-group-item active" href="./home.html">Home</a> */}
+                                
+                                {/* 在react中靠路由链接实现切换组件---编写路由连接 (此地址可以多，但不能少)*/}
+                                
+                                    
+                                    <MyNavLink to="/ddd/about">About</MyNavLink>
+                                    <MyNavLink to="/ddd/home">Home</MyNavLink>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-xs-6">
+                        <div className="panel">
+                            <div className="panel-body">
+                               
+                                    {/* 注册路由 */}
+                                    <Switch>
+                                        <Route path='/ddd/about' component={About}></Route>
+                                        <Route path='/ddd/home' component={Home}></Route>
+                                        <Redirect to='/ddd/about'></Redirect>
+                                    </Switch>
+                                    
+                               
+                                
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
         )
     }
